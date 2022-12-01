4b52b22f7e69b76e9b1b0a81c9bb7bcb11d0d9b9
dystewart <dystewar@redhat.com>
2022-12-01 14:33:52 -0500

    Adjust Dockerfile and include script check-vault-seal.sh in image

--- docker/check-vault-seal.sh
+++ docker/check-vault-seal.sh
@@ -3,7 +3,7 @@ set -ue
 
 echo "Checking vault seal status..."
 
-oc exec -i nerc-vault-0 -- vault status --format=json > /tmp/sealed.json
+kubectl exec -i nerc-vault-0 -- vault status --format=json > /tmp/sealed.json
 
 sealed=$(cat /tmp/sealed.json | yq -r '.sealed')
 echo "Sealed: $sealed"
